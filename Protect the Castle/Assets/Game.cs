c80da9f22348b303c65595fb752d44d20d97107a
Eemeli Syynimaa <EemeliSyynimaa@users.noreply.github.com>
2016-03-17 20:56:34 +0200

    Explosion update.

--- Protect the Castle/Assets/Game.cs
+++ Protect the Castle/Assets/Game.cs
@@ -2,59 +2,63 @@
 using System.Collections;
 
 public class Game : MonoBehaviour
+{
+    private GUIText scoreText;
+    private int score;
+    public float timer;
+    public int multiplier;
+    public int newScoreValue;
+    public bool gameOver;
+    public bool restart;
+    public bool exit;
+
+    void Start()
     {
-        private GUIText scoreText;
-        private int score;
-        public float timer;
-        public int multiplier;
-        public int newScoreValue;
-        public bool gameOver;
-        public bool restart;
-        public bool exit;
-        void Start()
-        {
-            scoreText = null;
-            score = 0;
-            timer = 0;
-            multiplier = 1;
-            gameOver = false;
-            restart = false;
-            exit = false;
-            GameController();
-        }
-        public void GameController()
-        {
+        scoreText = null;
+        score = 0;
+        timer = 0;
+        multiplier = 1;
+        gameOver = false;
+        restart = false;
+        exit = false;
+        GameController();
+    }
 
-        }
-        public void AddScore(int newScoreValue)
+    public void GameController()
+    {
+
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        //scoreText.text = "Score: " + score.ToString();
+        if ((Time.time - timer) >= 1)
         {
-            score += newScoreValue;
-        }
-        // Update is called once per frame
-        public void Update()
-        {
+            multiplier += 1;
             timer = Time.time;
-            scoreText.text = "Score: " + score;
-            if (timer >= 10)
-            {
-                multiplier += 1;
-                timer = 0;
-            }
-            // vaiko NumBlocks ???
+        }
+        // vaiko NumBlocks ???
 
-            newScoreValue = 10 * multiplier;
+        newScoreValue = 10 * multiplier;
             
-            AddScore(newScoreValue);
-        }
-    void OnGUI()
-        {
-            GUI.Label(new Rect(10, 10, 100, 100), scoreText.text);
+        AddScore(newScoreValue);
+    }
 
-        }
+    void OnGUI()
+    {
+        //GUI.Label(new Rect(10, 10, 100, 100), scoreText.text);
+    }
+
     public void IsHit()
-        {
-            multiplier = 1;
-            timer = 0;
-        }
+    {
+        multiplier = 1;
+        timer = Time.time;
+    }
 }
 
