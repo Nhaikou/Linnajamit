3da3b82b010624828f42b944e6c3dcf0b3d9d9e1
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-03-17 20:01:10 +0200

    sprite pictures added

--- Protect the Castle/Assets/Turn.cs
+++ Protect the Castle/Assets/Turn.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Turn : MonoBehaviour {
+public class Turn : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start ()
+    private Vector2 theScale;
+
+    // Use this for initialization
+    void Start()
     {
-	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        theScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
     {
-	
-	}
+        Vector2 velocity = GetComponentInParent<Rigidbody2D>().velocity;
+
+        if (velocity.x > 0)
+        {
+            theScale.x *= -1;
+        }
+        else
+        {
+
+        }
+    }
 }
