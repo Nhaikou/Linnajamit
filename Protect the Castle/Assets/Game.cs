47130a7f75d198de345d7742aa95b9a4ae76cdbc
Aleksi Nissinen <nissinena@elisanet.fi>
2016-03-17 19:10:18 +0200

    Scorecountterin päivitystä

--- Protect the Castle/Assets/Game.cs
+++ Protect the Castle/Assets/Game.cs
@@ -3,21 +3,20 @@ using System.Collections;
 
 public class Game : MonoBehaviour
     {
-        public GUIText scoreText;
+        private GUIText scoreText;
         private int score;
         public float timer;
         public int multiplier;
         public int newScoreValue;
-        public bool isHit;
         public bool gameOver;
         public bool restart;
         public bool exit;
         void Start()
         {
+            scoreText = null;
             score = 0;
             timer = 0;
             multiplier = 1;
-            isHit = false;
             gameOver = false;
             restart = false;
             exit = false;
@@ -25,33 +24,37 @@ public class Game : MonoBehaviour
         }
         public void GameController()
         {
-            while (!exit)
-            {
 
-            }
-
-            UpdateScore();
         }
         public void AddScore(int newScoreValue)
         {
             score += newScoreValue;
-            UpdateScore();
         }
         // Update is called once per frame
-        void UpdateScore()
+        public void Update()
         {
+            timer = Time.time;
             scoreText.text = "Score: " + score;
-            timer += 1;
             if (timer >= 10)
+            {
                 multiplier += 1;
+                timer = 0;
+            }
             // vaiko NumBlocks ???
 
             newScoreValue = 10 * multiplier;
-
-            if (isHit == true)
-                multiplier = 1;
-
+            
             AddScore(newScoreValue);
+        }
+    void OnGUI()
+        {
+            GUI.Label(new Rect(10, 10, 100, 100), scoreText.text);
+
+        }
+    public void IsHit()
+        {
+            multiplier = 1;
+            timer = 0;
         }
 }
 
